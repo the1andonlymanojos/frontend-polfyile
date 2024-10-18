0cba073a3a222b5e00360773bdbcfbb73c9db1ee
muskan-rathore <muskanrathoregwl9@gmail.com>
2024-10-18 11:49:46 +0530

    Added Services Page

---
+++ src/pages/Help.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Footer from "../components/Helpfooter";
+import Hero from "../components/HelpMain";
+import Header from "../components/header";
+import backgroundImage from "../components/img/background.svg";
+
+const Help = () => {
+  return (
+    <div
+      className="flex overflow-auto flex-col bg-white bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        height: '100vh',  // Full viewport height
+        width: '100vw',   // Full viewport width
+        backgroundSize: 'cover',  // Ensures the image covers the entire container
+        backgroundPosition: 'center',  // Centers the image
+      }}
+    >
+      {/* Content */}
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+
+  );
+};
+
+export default Help;
